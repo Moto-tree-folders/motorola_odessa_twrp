c2c376ef7b7e7260a4397fcfa993ef9c85a9e22c
Beta Soft <betaxab@gmail.com>
2021-01-16 05:49:58 +0800

    odessa: update misc    --adapt to odessa

--- twrp_odessa.mk
+++ twrp_odessa.mk
@@ -1,11 +1,11 @@
 #
-# Copyright 2017 The Android Open Source Project
+# Copyright 2021 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,18 +16,12 @@
 
 # Release name
 PRODUCT_RELEASE_NAME := odessa
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += fastbootd
 # Custom vendor used in build tree (automatically taken from prefix of this filename)
 CUSTOM_VENDOR := $(lastword $(subst /, ,$(firstword $(subst _, ,$(firstword $(MAKEFILE_LIST))))))
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
-
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/$(CUSTOM_VENDOR)/config/common.mk)
-
-# Inherit from hardware-specific part of the product configuration
-$(call inherit-product, device/motorola/odessa/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := odessa
@@ -35,12 +29,10 @@ PRODUCT_NAME := $(CUSTOM_VENDOR)_$(PRODUCT_DEVICE)
 PRODUCT_BRAND := Motorola
 PRODUCT_MODEL := odessa
 PRODUCT_MANUFACTURER := Motorola
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/$(PRODUCT_BRAND)/$(PRODUCT_DEVICE)/device.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="motorola G9 Plus" \
     BUILD_PRODUCT="motorola G9 Plus" \
     TARGET_DEVICE="motorola G9 Plus"
-
-# HACK: Set vendor patch level
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31
